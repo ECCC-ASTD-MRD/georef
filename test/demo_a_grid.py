d975d3a31e6116a6af9ee5ab9bd2a65eb940defe
Maude Preville <maude.preville@polymtl.ca>
2020-05-22 13:46:32 +0000

    ajoute code pour appel de cezsint par libgeoref

--- test/demo_a_grid.py
+++ test/demo_a_grid.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import rpnpy.librmn.all as rmn
+import libgeoref.interp as libgeoref
 import stage_2020
 
 def gen_params(nlon, nlat, hemisphere):
@@ -68,7 +69,8 @@ def error(params, data):
     out_gid = rmn.defGrid_L(nlon, nlat, lat0, lon0, dlat, dlon)
     out_lalo = rmn.gdll(out_gid)
 
-    out_data = rmn.ezsint(out_gid, gid, data)
+    out_data = libgeoref.wrapper_ezsint(out_gid, gid, data)
+    #out_data = rmn.ezsint(out_gid, gid, data)
     true_data = np.sin(np.pi*out_lalo['lon']/180)\
         *np.sin(np.pi*out_lalo['lat']/90)
     return np.linalg.norm(out_data - true_data)
@@ -80,7 +82,7 @@ def main():
     nlat = 180//30
     hemisphere = 'global'
     params = gen_params(nlon, nlat, hemisphere)
-    plot_grid(params)
+    #plot_grid(params)
 
     # Fine mesh required to recognize analytic 3-D surface
     nlon = 360//5
@@ -91,10 +93,11 @@ def main():
     lalo = rmn.gdll(gid)
     data = np.sin(np.pi*lalo['lon']/180)*np.sin(np.pi*lalo['lat']/90)
 
-    plot_data(params, data)
+    #plot_data(params, data)
 
-    stage_2020.write_fst(data, params, 'a_grid.fst')
-    print(error(params, data))
+    #stage_2020.write_fst(data, params, 'a_grid.fst')
+    #print(error(params, data))
+    error(params, data)
 
 if __name__ == "__main__":
     main()
