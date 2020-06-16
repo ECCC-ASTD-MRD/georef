385a63f6177f943ea7e88a8a7dd9461a9d2c1b58
Kristjan Onu <kristjan.onu@canada.ca>
2020-06-16 16:18:23 -0400

    Ajoute demo pour grille M

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Cette branche ajoute des fichiers Standard RPN avec des données sinus.
+
+# Exemples
+
+```shell
+ssh eccc-ppp4.science.gc.ca
+. r.load.dot comm/eccc/all/opt/intelcomp/intelpsxe-cluster-19.0.3.199 \
+  rpn/libs/19.5 \
+  rpn/MIG/ENV/rpnpy/2.1.0
+cd test
+./demo_m_grid.py
+```
---
+++ test/demo_m_grid.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+'Démonstration pour grille de type M'
+
+import os
+import pathlib
+import shutil
+import numpy as np
+import rpnpy.librmn.all as rmn
+
+def main():
+    'Ajoute champs sinus à fichier Standard RPN'
+
+    pathlib.Path('out').mkdir(exist_ok=True)
+    shutil.copyfile(os.path.join('in', 'm.std'), os.path.join('out', 'm.std'))
+    funit = rmn.fstopenall(os.path.join('out', 'm.std'), rmn.FST_RW)
+    lat = rmn.fstlir(funit, nomvar='^^')['d']
+    lon = rmn.fstlir(funit, nomvar='>>')['d'][0]
+    hb = rmn.fstlir(funit, nomvar='hb')
+    x = hb
+    x['nomvar'] = 'x'
+    x['etiket'] = ''
+    x['d'] = np.sin(np.pi*lon/180)*np.sin(np.pi*lat/90)
+    rmn.fstecr(funit, x)
+    rmn.fstcloseall(funit)
+
+if __name__ == "__main__":
+
+    main()
