c4a95f9579c83247797c51cc58be954b72ecaf3b
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2020-06-19 20:35:05 +0000

    Fixed wrong install path. Added missing librmn link.

--- test/libgeoref/__init__.py
+++ test/libgeoref/__init__.py
@@ -3,9 +3,8 @@
 import ctypes as ct
 import os
 
-X = 'libgeoref_0.1.0-' + os.environ['COMP_ARCH'] + '_' + os.environ['BASE_ARCH']
+X = 'libgeoref_0.1.0-' + os.environ['COMP_ARCH'] + '_' + os.environ['ORDENV_PLAT']
 
 libgeoref = ct.cdll.LoadLibrary(
-    os.path.join(os.environ['SSM_DEV'], 'workspace', X,
-                 os.environ['SSM_DEV'][1:], 'workspace', X, 'lib',
+    os.path.join(os.environ['SSM_DEV'], 'workspace', X,'lib',
                  'libgeoref.so'))
