101bacbb6d74fa0fac6d2e17daf4603d6973714d
Maude Preville <maude.preville@polymtl.ca>
2020-06-22 14:38:35 +0000

    Enleve code en commentaire de ezscint.h

--- src_ez/ezscint.h
+++ src_ez/ezscint.h
@@ -56,7 +56,6 @@
 #define FICHIER         1
 #define MEMOIRE         2
 
-// #define NZONES             5
 #define DEHORS             0
 #define AU_NORD            1
 #define AU_SUD             2
@@ -118,112 +117,6 @@
 #define YES             1
 #define NO              0
 
-// typedef struct
-// {
-//   wordint npts;               /* nombre de points */
-//   ftnfloat *x;                /* vecteur de coordonnees x */
-//   ftnfloat *y;                /* vecteur de coordonnees y */
-//   wordint *idx;               /* indice du point dans le champ de destination */
-// } _zone;
-
-// typedef struct
-// {
-//   wordint n_wts;          /* nombre de poids */
-//   ftnfloat *xx, *yy;
-//   ftnfloat *lat, *lon;
-//   ftnfloat *wts;              /* tableau de poids */
-//   wordint *mask, *idx;               /* indice du point dans le champ de destination */
-// } _ygrid;                     /* Grille Y */
-
-// typedef struct
-// {
-//   wordint flags;
-//   ftnfloat *lat_rot, *lon_rot, *lat_true, *lon_true;
-//   ftnfloat *sinlat_rot, *coslat_rot, *sinlon_rot, *coslon_rot;
-//   ftnfloat *sinlat_true, *coslat_true, *sinlon_true, *coslon_true;
-//   ftnfloat r[9], ri[9];
-// } _gemgrid;
-
-// typedef struct
-// {
-//   wordint flags,yyflags;
-//   wordint use_sincos_cache;
-//   wordint gdin;
-//   wordint next_gdin;
-//   ftnfloat valpolesud, valpolenord;
-//   ftnfloat *x, *y;
-//   wordint *mask_in, *mask_out;
-//   ftnfloat *yin_maskout,*yan_maskout;
-//   ftnfloat *yinlat,*yinlon,*yanlat,*yanlon;
-//   ftnfloat *yin2yin_lat,*yin2yin_lon,*yan2yin_lat,*yan2yin_lon;
-//   ftnfloat *yin2yan_lat,*yin2yan_lon,*yan2yan_lat,*yan2yan_lon;
-//   ftnfloat *yin2yin_x,*yin2yin_y,*yan2yin_x,*yan2yin_y;
-//   ftnfloat *yin2yan_x,*yin2yan_y,*yan2yan_x,*yan2yan_y;
-//   wordint yincount_yin,yancount_yin,yincount_yan,yancount_yan;
-//   _gemgrid gemin, gemout;
-//   _ygrid ygrid;
-//   _zone zones[NZONES];
-// }_gridset;
-
-// typedef struct
-//    {
-//    int child;
-//    int childOf;
-//    int parent;
-//    int niOffset, njOffset;
-//    int sister;
-//    int assembly;
-//    int *parentOf;
-//    int *sisterOf;
-//    }_sousgrille;
-
-// typedef struct
-//   {
-//   wordint  ip1, ip2, ip3;
-//   wordint date;
-//   wordint npas, deet, nbits;
-//   wordint hemisphere,axe_y_inverse;
-//   ftnfloat xg[16], xgref[16];
-//   wordint  ig[16], igref[16];
-//   char fst_grtyp[4],fst_grref[4];
-//   wordint key_ax, key_ay;
-//   char nomvarx[8];
-//   char nomvary[8];
-//   char typvarx[4];
-//   char typvary[4];
-//   char etiketx[16];
-//   char etikety[16];
-//   } _fstinfo;
-
-// typedef struct
-// {
-//   wordint index;
-//   wordint flags;
-//   wordint i1, i2, j1, j2;
-//   wordint ni,nj;
-//   wordint nig, nxg;
-//   wordint ni_ax, nj_ay;
-//   wordint extension;
-//   wordint needs_expansion;
-//   wordint access_count;
-//   wordint structured;
-//   wordint next_gd;
-//   wordint n_gdin, next_gdin, idx_last_gdin, n_gdin_for;
-//   wordint log_chunk_gdin, log_chunk_gdin_for;
-//   wordint *gdin_for, *mask;
-//   wordint nsubgrids,mymaskgrid;
-//   wordint mymaskgridi0,mymaskgridi1;
-//   wordint mymaskgridj0,mymaskgridj1;
-//   wordint *subgrid;
-//   ftnfloat *lat, *lon;
-//   ftnfloat *ax, *ay;
-//   ftnfloat *ncx, *ncy;
-//   char grtyp[4], grref[4];
-//   _fstinfo fst;
-//   _gridset *gset;
-// } _Grille;
-
-
 typedef struct
 {
   wordint  damage_control;
@@ -245,8 +138,6 @@ typedef struct
   ftnfloat valeur_extrap;
 }_groptions;
 
-//  extern TGeoRef** Grille;
-//  extern TGeoRef** gr_list;
  extern wordint nGrilles;
  extern wordint nGrillesMax;
  extern wordint cur_log_chunk;
@@ -257,7 +148,6 @@ typedef struct
  extern wordint iset;
  extern wordint iset_gdin;
  extern wordint iset_gdout;
-//  extern _gridset* gridset;
  extern _groptions groptions;
 
  extern wordint log_chunks[];
