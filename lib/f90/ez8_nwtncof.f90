4db53a24227eba815e41819e1998018b016f52f8
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@ec.gc.ca>
2025-01-13 17:17:46 +0000

    Got rid of licence in file headers

--- lib/f90/ez8_nwtncof.f90
+++ lib/f90/ez8_nwtncof.f90
@@ -1,22 +1,3 @@
-
-! * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
-! *                          Environnement Canada
-! *
-! * This library is free software; you can redistribute it and/or
-! * modify it under the terms of the GNU Lesser General Public
-! * License as published by the Free Software Foundation,
-! * version 2.1 of the License.
-! *
-! * This library is distributed in the hope that it will be useful,
-! * but WITHOUT ANY WARRANTY; without even the implied warranty of
-! * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-! * Lesser General Public License for more details.
-! *
-! * You should have received a copy of the GNU Lesser General Public
-! * License along with this library; if not, write to the
-! * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-! * Boston, MA 02111-1307, USA.
-! */
    subroutine ez8_nwtncof(cx,cy,ax,ay,ni,nj,i1,i2,j1,j2,extension)
 !*******
 !     Auteur: Y. Chartier, drpn
