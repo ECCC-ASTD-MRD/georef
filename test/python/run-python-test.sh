e00823798c1b43d806b0b6fe3a9b2ae2661f8914
Philippe Carphin <philippe.carphin@ec.gc.ca>
2025-01-20 20:19:01 -0500

    Add shebang to run-python-test.sh

--- test/python/run-python-test.sh
+++ test/python/run-python-test.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env -S bash -o errexit -o nounset
+
 this_dir=$(cd $(dirname $0) && pwd)
 # Librmn with the python package must be loaded
 # This python package must be loaded.  This can either be the
