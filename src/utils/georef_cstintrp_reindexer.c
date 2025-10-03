f789d4a7c852fa44123d8aaa7d5f3e571144ec38
Philippe Carphin <philippe.carphin@ec.gc.ca>
2025-10-03 13:42:18 -0400

    WIP Adapt reindexer

--- src/utils/georef_cstintrp_reindexer.c
+++ src/utils/georef_cstintrp_reindexer.c
@@ -1,13 +1,22 @@
+// vim: et:ts=3:sw=3:sts=3
+
 #include <App.h>
 #include <GeoRef.h>
 #include <Def.h>
 #include <georef_build_info.h>
 
+
 #define APP_NAME "georef_reindexer"
 #define APP_DESC "ECCC/CMC RPN Index conversion tool"
 
-const char *NEMO_Vars[] = { "W001","W002","W003","W004", "I001","I002","I003","I004", "J001","J002","J003","J004", "NAVG","ANG","MASK" };
-typedef enum { W001,W002,W003,W004, I001,I002,I003,I004, J001,J002,J003,J004, NAVG,ANG,MASK } NEMO_Idx;
+//const char *NEMO_Vars[] = { "W001","W002","W003","W004", "I001","I002","I003","I004", "J001","J002","J003","J004", "NAVG","ANG","MASK" };
+// typedef enum { W001,W002,W003,W004, I001,I002,I003,I004, J001,J002,J003,J004, NAVG,ANG,MASK } NEMO_Idx;
+
+typedef enum {NAVG, ANG, MASK, NOTHER} Other;
+const char *OtherStr[NOTHER] = {"NAVG", "ANG", "MASK"};
+
+typedef enum {W, I, J, N} RecType;
+const char *FMT[J+1] = {"W%03d", "I%03d", "J%03d"};
 
 // Usage example call for CANSIPS
 // GEM_to NEMO: georef_cstintrp_reindexer -i /home/smco502/datafiles/constants/cmdn/cansips/atm_ocean//Grille_20240202/weights/weights_gem319x262_to_orca1_default_yin.std /home/smco502/datafiles/constants/cmdn/cansips/atm_ocean//Grille_20240202/weights/weights_gem319x262_to_orca1_default_yang.std -o ./atmos-ocean-grids.fstd -g OU -d 319 131 -b 2
@@ -16,30 +25,39 @@ typedef enum { W001,W002,W003,W004, I001,I002,I003,I004, J001,J002,J003,J004, NA
 #define I16_dest_t int32_t // dty: I 16 gets read into 32 bit
 #define I1_dest_t  int32_t // dty: I 1  gets read into 32 bit
 
-int ReIndex(char **In,char *Out,char* FromTo,int *OtherDims,int BDW, int Orca) {
+int ReIndex(char **In,char *Out,char* FromTo,int *OtherDims,int BDW, int Orca, int nb_weights) {
 
-   fst_record  rec[2][15];
+   const int  nsubgrid = (In[1] ? 2 : 1);
+   fst_record  rec[nsubgrid][nb_weights][N];
+   fst_record others[nsubgrid][3];
    fst_record  out=default_fst_record,ang=default_fst_record,crit=default_fst_record;
-   fst_file   *fin[2],*fout;
-   float      *data, *angle_data;
-   int         i=0,j=0,iy,jy,n=0,v=0,w=0,sz=0,idx,g,in,subgrid;
+   fst_file   *fin[nsubgrid],*fout;
+   float      *data_out, *angle_data_out;
+   int         i=0,j=0,iy,jy,n=0,v=0,w=0,idx,g,in;
    float       a;
-   I16_dest_t *iy_data[2][4];
-   I16_dest_t *jy_data[2][4];
-   double     *w_data[2][4];
-   I1_dest_t  *mask_data[2];
+   char       nomvar[FST_NOMVAR_LEN];
+   int        glb_ni[nsubgrid], glb_nj[nsubgrid], sz[nsubgrid];
+
+   // Arrays typed pointers for data of W, I, J input records
+   I16_dest_t *iy_data[nsubgrid][nb_weights];
+   I16_dest_t *jy_data[nsubgrid][nb_weights];
+   double     *w_data[nsubgrid][nb_weights];
+
+   // Arrays of typed pointers for NAVG, MASK, ANG input records
+   I1_dest_t  *mask_data[nsubgrid];
+   I16_dest_t *navg_data[nsubgrid];
+   float      *ang_data[nsubgrid];
+
+   // Other vars
    I16_dest_t *navg_in;
    I1_dest_t  *mask_in;
    float      *ang_in;
 
-   if (!(fin[0]=fst24_open(In[0],"R/O"))) {
-      App_Log(APP_ERROR,"Problems opening input file %s\n",In[0]);
-      return(FALSE);
-   }
-
-   if (In[1] && !(fin[1]=fst24_open(In[1],"R/O"))) {
-      App_Log(APP_ERROR,"Problems opening input file %s\n",In[1]);
-      return(FALSE);
+   for(int sg = 0; sg < nsubgrid; sg++){
+      if (!(fin[sg]=fst24_open(In[0],"R/O"))) {
+         App_Log(APP_ERROR,"Problems opening input file %s\n",In[0]);
+         return(FALSE);
+      }
    }
 
    if (!(fout=fst24_open(Out,"R/W"))) {
@@ -47,46 +65,55 @@ int ReIndex(char **In,char *Out,char* FromTo,int *OtherDims,int BDW, int Orca) {
       return(FALSE);
    }
 
-   // Create the desination grid
-   for(n=W001;n<=MASK;n++){
-      rec[0][n]=rec[1][n]=default_fst_record;
-      strncpy(crit.nomvar,NEMO_Vars[n],FST_NOMVAR_LEN);
-      if (!fst24_read(fin[0],&crit,NULL,&rec[0][n])) {
-         App_Log(APP_ERROR,"Could not read %s from %s\n",NEMO_Vars[n],In[0]);
-         return(FALSE);
+   for(int sg = 0; sg < nsubgrid; sg++){
+      for(n=0; n<nb_weights; n++){
+         for(int t = W; t<=J; t++){
+            snprintf(crit.nomvar, FST_NOMVAR_LEN, FMT[t], n+1);
+            fprintf(stderr, "sg=%d, n=%d, t=%d, crit.nomvar='%s'\n",sg, n,t,crit.nomvar);
+            rec[sg][n][t] = default_fst_record;
+            if(!fst24_read(fin[sg], &crit, NULL, &rec[sg][n][t])){
+               App_Log(APP_ERROR,"Could not read %s from %s\n", nomvar,In[0]);
+               return(FALSE);
+            }
+         }
+         // Cast by assigning
+          w_data[sg][n] = rec[sg][n][W].data;
+         iy_data[sg][n] = rec[sg][n][I].data;
+         jy_data[sg][n] = rec[sg][n][J].data;
       }
-      if (In[1] && (!fst24_read(fin[1],&crit,NULL,&rec[1][n]))) {
-         App_Log(APP_ERROR,"Could not read %s from %s\n",NEMO_Vars[n],In[1]);
-         return(FALSE);
+   }
+
+   for(n = NAVG; n <= MASK; n++){
+      strncpy(crit.nomvar, OtherStr[n],FST_NOMVAR_LEN);
+      for(int sg = 0; sg < nsubgrid; sg++){
+         others[sg][n] = default_fst_record;
+         fprintf(stderr, "sg=%d, n=%d, crit.nomvar='%s'\n", sg, n, crit.nomvar);
+         if(!fst24_read(fin[sg], &crit, NULL, &others[sg][n])){
+            App_Log(APP_ERROR,"Could not read %s from %s\n", nomvar,In[0]);
+            return(FALSE);
+         }
       }
    }
-
-   // Transfer void* data pointers into pointers of the proper type for
-   // indexing into the arrays.
-   for(n=W001; n<=W004; n++){
-       for(subgrid = 0; subgrid <= (In[1] ? 1 : 0); subgrid++){
-            w_data[subgrid][n] = rec[subgrid][n].data;
-           iy_data[subgrid][n] = rec[subgrid][I001+n].data;
-           jy_data[subgrid][n] = rec[subgrid][J001+n].data;
-       }
+   // Cast by assigning
+   for(int sg = 0; sg < nsubgrid; sg++){
+      mask_data[sg] = others[sg][MASK].data;
+      navg_data[sg] = others[sg][NAVG].data;
+      ang_data[sg]  = others[sg][ANG].data;
+      glb_ni[sg] = rec[sg][0][0].ni;
+      glb_nj[sg] = rec[sg][0][0].nj;
+      sz[sg] = glb_ni[sg]*glb_nj[sg];
    }
 
-   mask_data[0] = rec[0][MASK].data;
-   if(In[1]){
-       mask_data[1] = rec[1][MASK].data;
-   }
-
-   sz=rec[0][0].ni*rec[0][0].nj;
-   data=(float*)malloc(sz*2*15*sizeof(float));
-   angle_data=(float*)malloc(sz*2*3*sizeof(float));
+   data_out=(float*)malloc(sz[0]*2*(3*(nb_weights+1))*sizeof(float));
+   angle_data_out=(float*)malloc(sz[0]*2*3*sizeof(float));
 
    //Yin
-   navg_in = rec[0][NAVG].data;
-   ang_in = rec[0][ANG].data;
+   navg_in = navg_data[0];
+   ang_in = ang_data[0];
    mask_in = mask_data[0];
    idx=0;
-   for(j=0;j<rec[0][0].nj;j++) {
-      for(i=0;i<rec[0][0].ni;i++,idx++) {
+   for(j=0;j<glb_nj[0];j++) {
+      for(i=0;i<glb_ni[0];i++,idx++) {
          if ((g=navg_in[idx]) > 0) {
             // Check if inside core grid
             in=TRUE;
@@ -105,18 +132,18 @@ int ReIndex(char **In,char *Out,char* FromTo,int *OtherDims,int BDW, int Orca) {
                in=FALSE;
             }
             if (in) {
-               data[v++]=i;
-               data[v++]=j;
+               data_out[v++]=i;
+               data_out[v++]=j;
                a=-ang_in[idx]*M_PI/180;
-               angle_data[w++]=cos(a);
-               angle_data[w++]=sin(a);
+               angle_data_out[w++]=cos(a);
+               angle_data_out[w++]=sin(a);
                for(n=0;n<g;n++){
                   iy=iy_data[0][n][idx]-1;
-                  data[v++]= (Orca && iy==0) ? OtherDims[0]-2 : ((Orca && iy==OtherDims[0]-1) ? 1 : iy);
-                  data[v++]= jy_data[0][n][idx]-1;
-                  data[v++]= w_data[0][n][idx];
+                  data_out[v++]= (Orca && iy==0) ? OtherDims[0]-2 : ((Orca && iy==OtherDims[0]-1) ? 1 : iy);
+                  data_out[v++]= jy_data[0][n][idx]-1;
+                  data_out[v++]= w_data[0][n][idx];
                } 
-               data[v++]=angle_data[w++]=REF_INDEX_SEPARATOR;
+               data_out[v++]=angle_data_out[w++]=REF_INDEX_SEPARATOR;
             }
          }
       }
@@ -124,12 +151,12 @@ int ReIndex(char **In,char *Out,char* FromTo,int *OtherDims,int BDW, int Orca) {
 
    // Yang
    if (In[1]) {
-      navg_in = rec[1][NAVG].data;
-      ang_in = rec[1][ANG].data;
+      navg_in = navg_data[1];
+      ang_in = ang_data[1];
       mask_in = mask_data[1];
       idx=0;
-      for(j=0;j<rec[1][0].nj;j++) {
-         for(i=0;i<rec[1][0].ni;i++,idx++) {
+      for(j=0;j<glb_nj[1];j++) {
+         for(i=0;i<glb_ni[1];i++,idx++) {
             if ((g=navg_in[idx]) > 0) {
                // Check if inside core grid
                in=TRUE;
@@ -148,28 +175,28 @@ int ReIndex(char **In,char *Out,char* FromTo,int *OtherDims,int BDW, int Orca) {
                   in=FALSE;
                }
                if (in) {
-                  data[v++]=i;
-                  data[v++]=(FromTo[0]=='U')?j+rec[0][0].nj:j;
+                  data_out[v++]=i;
+                  data_out[v++]=(FromTo[0]=='U')?j+glb_nj[0]:j;
                   a=-ang_in[idx]*M_PI/180;
-                  angle_data[w++]=cos(a);
-                  angle_data[w++]=sin(a);
+                  angle_data_out[w++]=cos(a);
+                  angle_data_out[w++]=sin(a);
                   for(n=0;n<g;n++){
                      iy=iy_data[1][n][idx]-1;
-                     data[v++]= (Orca && iy==0) ? OtherDims[0]-2 : ((Orca && iy==OtherDims[0]-1) ? 1 : iy);
+                     data_out[v++]= (Orca && iy==0) ? OtherDims[0]-2 : ((Orca && iy==OtherDims[0]-1) ? 1 : iy);
                      jy=jy_data[1][n][idx]-1;
-                     data[v++]= (FromTo[0]=='O') ? jy+OtherDims[1] : jy;
-                     data[v++]= w_data[1][n][idx];
+                     data_out[v++]= (FromTo[0]=='O') ? jy+OtherDims[1] : jy;
+                     data_out[v++]= w_data[1][n][idx];
                   }
-                  data[v++]=angle_data[w++]=REF_INDEX_SEPARATOR;
+                  data_out[v++]=angle_data_out[w++]=REF_INDEX_SEPARATOR;
                } 
             }  
          }
       }
    }   
-   data[v++]=angle_data[w++]=REF_INDEX_END;
+   data_out[v++]=angle_data_out[w++]=REF_INDEX_END;
 
-   out.data=data;
-   ang.data=angle_data;
+   out.data=data_out;
+   ang.data=angle_data_out;
    out.data_type = ang.data_type = FST_TYPE_REAL_IEEE;
    out.data_bits = ang.data_bits = 32;
    out.pack_bits = ang.data_bits = 32;
@@ -213,11 +240,12 @@ int ReIndex(char **In,char *Out,char* FromTo,int *OtherDims,int BDW, int Orca) {
 
 int main(int argc, char *argv[]) {
 
-   int         ok=0,m=-1,code=0,odim[2]={0,0},bdw=0,orca=0;
+   int         ok=0,code=0,odim[2]={0,0},bdw=0,orca=0, nw=4;
    char        *in[2]={ NULL, NULL },*out=NULL,*ft=NULL;
  
    TApp_Arg appargs[]=
       { { APP_CHAR,   in,    2,             "i", "input",  "Input file" },
+        { APP_INT32, &nw,    1,             "n", "nb-weights", "Number of weights" },
         { APP_CHAR,  &out,   1,             "o", "output", "Output file" },
         { APP_CHAR,  &ft,    1,             "g", "grid",   "Grid types from->to (ie: U->O = 'OU')" },
         { APP_INT32, &odim,  2,             "d", "dims",   "NI,NJ dimension of the other (sub)grid " },
@@ -245,7 +273,7 @@ int main(int argc, char *argv[]) {
    App_Start();
 
    if (code!=EXIT_FAILURE) {
-      ok=ReIndex(in,out,ft,odim,bdw,orca);
+      ok=ReIndex(in,out,ft,odim,bdw,orca, nw);
    }
    code=App_End(ok?0:EXIT_FAILURE);
  
